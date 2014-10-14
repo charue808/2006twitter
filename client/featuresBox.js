b23aadf36c0575f03f54ae90768d843659541c0a
charue808 <charlesgum@gmail.com>
2014-10-14 02:51:42 +0000

    second commit

---
+++ client/featuresBox.js
@@ -0,0 +1,15 @@
+Template.featuresBox.events({
+  'click #button1': function () {
+     document.getElementById('theImage').src="/image/Twitterbox1.png";
+     document.getElementById('thetextBox').src="/image/wigflip-ds (2).png";
+    },
+  'click #button2': function () {
+     document.getElementById('theImage').src="/image/twitterinvite2.png";
+     document.getElementById('thetextBox').src="/image/wigflip-ds (4).png";
+     
+    },
+  'click #button3': function () {
+     document.getElementById('theImage').src="/image/twitter3.png";
+     document.getElementById('thetextBox').src="/image/wigflip-ds (3).png";
+    },
+})
